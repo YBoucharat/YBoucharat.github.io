927783f80702ff8b1f1817fe323e3ca423da3fdd
YBoucharat <162440482+YBoucharat@users.noreply.github.com>
2026-05-05 13:05:48 +0000

    Deploying to gh-pages from @ YBoucharat/YBoucharat.github.io@a46d4e09980bc8b608a3da51c57500280175a7a1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -496,9 +496,9 @@ ninja.data = [{
           description: "Description of the RoSETTA code",
           section: "Projects",handler: () => {
               window.location.href = "/projects/RoSETTA/";
-            },},{id: "teachings-biogeodynamics-italy",
-          title: 'Biogeodynamics_italy',
-          description: "I developped this practical for master 1 and 2 students as an introduction to coral reef terraces dynamics, highlighting the interaction between Quaternary sea-level change, vertical movements and erosion/coral reef growth. The practical is then centered on the utilisation of the REEF code.",
+            },},{id: "teachings-mountain-building-climate-and-biodiversity",
+          title: 'Mountain building, climate and biodiversity',
+          description: "8 days of fieldwork in the Appenines to reconstruct the history of the Appenines mountain chain, from Eocene to present day, with links to other roll-backs systems in the Mediterranean, basin (de-)connections, climate and evolution of the biosphere.",
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/Biogeodynamics_Italy/";
             },},{id: "teachings-reef-code-practical",
